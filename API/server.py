60a790cff94401d56f233aaf11ea1ead454ea364
bakytk <bakytkh@outlook.com>
2021-06-11 21:48:22 +0300

    commit

--- API/server.py
+++ API/server.py
@@ -1,9 +1,9 @@
 
-import redis
+#import redis
 from flask import Flask
 
 app = Flask(__name__)
-redis = redis.Redis(host='redis', port=6379, db=0)
+#redis = redis.Redis(host='redis', port=6379, db=0)
 
 @app.route('/')
 def hello_world():
@@ -11,12 +11,12 @@ def hello_world():
     
 @app.route('/visitor') 
 def visitor():
-    redis.incr('visitor')
-    visitor_num = redis.get('visitor').decode("utf-8")
-    return "Visitor: %s" % (visitor_num)
+    #redis.incr('visitor')
+    #visitor_num = redis.get('visitor').decode("utf-8")
+    return "Visitor: %s" % (1)
     
 @app.route('/visitor/reset')  
 def reset_visitor():
-    redis.set('visitor', 0)
-    visitor_num = redis.get('visitor').decode("utf-8")
-    return "Visitor is reset to %s" % (visitor_num)
+    #redis.set('visitor', 0)
+    #visitor_num = redis.get('visitor').decode("utf-8")
+    return "Visitor is reset to %s" % (2)
